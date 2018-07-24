30aeba289467b146759f4534cb28130bd59463d5
Vagner Praia da Silva <vagnerpraia@gmail.com>
2018-07-24 20:51:25 -0300

    Definindo constantes

--- ola_mundo.go
+++ ola_mundo.go
@@ -16,13 +16,19 @@ func main() {
 
 	// Definição de variável com tipagem automática
 	author := "Vagner Praia"
+	print_author := true
 
 	text_end = "Fim do olá mundo"
+
+	// Definindo constantes
+	const ano int = 2018
 
 	fmt.Println("Texto inicial: " + text_start)
 	fmt.Println(count)
 	fmt.Println("Texto final: " + text_end)
+	fmt.Println(print_author)
 	fmt.Println("Author: " + author)
+	fmt.Println(ano)
 
 	// Comentário 2
 }
