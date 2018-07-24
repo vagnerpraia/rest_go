6863036e62205bb3882141f66bac7be4f0a69c5d
Vagner Praia da Silva <vagnerpraia@gmail.com>
2018-07-24 20:45:21 -0300

    Declaração de variáveis

--- ola_mundo.go
+++ ola_mundo.go
@@ -9,13 +9,20 @@ func main() {
 		Comentário 1
 	*/
 
-	var text_start string = "Olá mundo"
+	// Definição de variável com tipagem estática
+	var text_start string = "Inicío do olá mundo"
 	var text_end string
 	var count int = 4546846 * 3
+
+	// Definição de variável com tipagem automática
+	author := "Vagner Praia"
+
+	text_end = "Fim do olá mundo"
 
 	fmt.Println("Texto inicial: " + text_start)
 	fmt.Println(count)
 	fmt.Println("Texto final: " + text_end)
+	fmt.Println("Author: " + author)
 
 	// Comentário 2
 }
