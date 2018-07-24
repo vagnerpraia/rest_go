fcb0a5a12c275954740331fccb02f4fdacff3369
Vagner Praia da Silva <vagnerpraia@gmail.com>
2018-07-24 19:23:49 -0300

    Programa olá mundo

--- ola_mundo.go
+++ ola_mundo.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
+	/*
+		Comentário 1
+	*/
 	fmt.Println("Olá mundo")
+	time.Sleep(time.Second * 1)
+	fmt.Println(1)
+	time.Sleep(time.Second * 1)
+	fmt.Println(2)
+	time.Sleep(time.Second * 1)
+	fmt.Println(3)
+	time.Sleep(time.Second * 1)
+	fmt.Println("Já")
+	// Comentário 2
 }
