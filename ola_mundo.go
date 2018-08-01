a54381362ea98d55047a4298bc34fb2855ca766e
Vagner Praia da Silva <vagnerpraia@gmail.com>
2018-08-01 12:51:41 -0300

    Exemplo de uso de switch

--- ola_mundo.go
+++ ola_mundo.go
@@ -183,6 +183,27 @@ func main() {
 	for _, produto := range produtos {
 		fmt.Println("Produto: " + produto)
 	}
+
+	// Exemplo de switch
+	dia_semana := 5
+	switch dia_semana {
+	case 1:
+		fmt.Println("Domingo")
+	case 2:
+		fmt.Println("Segunda")
+	case 3:
+		fmt.Println("Terça")
+	case 4:
+		fmt.Println("Quarta")
+	case 5:
+		fmt.Println("Quinta")
+	case 6:
+		fmt.Println("Sexta")
+	case 7:
+		fmt.Println("Sábado")
+	default:
+		fmt.Println("Erro")
+	}
 }
 
 func mostrarNome(carro Carro) {
