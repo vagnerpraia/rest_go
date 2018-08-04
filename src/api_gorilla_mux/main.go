63f89c1ae225f5a0ff646d9f5a9065e23cea8535
Vagner Praia da Silva <vagnerpraia@gmail.com>
2018-08-04 04:00:00 -0300

    Modularizando

--- src/api_gorilla_mux/main.go
+++ src/api_gorilla_mux/main.go
@@ -1,20 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/gorilla/mux"
 )
-
-var usuarios = Usuarios{
-	Usuario{1, "Maria da Silva", "maria@mailtest.com", "123456"},
-	Usuario{2, "João da Silva", "joao@mailtest.com", "456789"},
-	Usuario{3, "Paulo da Silva", "paulo@mailtest.com", "789123"},
-}
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
@@ -29,43 +21,10 @@ func main() {
 	log.Fatal(server)
 }
 
-func obterIndex(response http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(response, "API Rest Go com gorilla/mux")
-}
-
-func obterUsuarios(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	enc := json.NewEncoder(w)
-	enc.Encode(usuarios)
-}
-
-func obterUsuario(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-
-	id, error := strconv.Atoi(params["id"])
-	showError(error)
-
-	id -= 1
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-
-	enc := json.NewEncoder(w)
-	enc.Encode(usuarios[id])
+func obterIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "API Rest Go com gorilla/mux")
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-
-	id, error := strconv.Atoi(params["id"])
-	showError(error)
-
-	id -= 1
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-
-	enc := json.NewEncoder(w)
-	enc.Encode(usuarios[id])
+	fmt.Fprintf(w, "Login")
 }
--- src/api_gorilla_mux/usuario.go
+++ src/api_gorilla_mux/usuario.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
 type Usuario struct {
 	Id    int    `json:"id"`
 	Nome  string `json:"nome"`
@@ -8,3 +16,31 @@ type Usuario struct {
 }
 
 type Usuarios []Usuario
+
+var usuarios = Usuarios{
+	Usuario{1, "Maria da Silva", "maria@mailtest.com", "123456"},
+	Usuario{2, "João da Silva", "joao@mailtest.com", "456789"},
+	Usuario{3, "Paulo da Silva", "paulo@mailtest.com", "789123"},
+}
+
+func obterUsuarios(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	enc := json.NewEncoder(w)
+	enc.Encode(usuarios)
+}
+
+func obterUsuario(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	id, error := strconv.Atoi(params["id"])
+	showError(error)
+
+	id -= 1
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	enc := json.NewEncoder(w)
+	enc.Encode(usuarios[id])
+}
