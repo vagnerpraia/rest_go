a1a7cb3b8218a71507c785f8ac0b76bc55af1f5b
Vagner Praia da Silva <vagnerpraia@gmail.com>
2018-08-04 01:02:36 -0300

    Organização do main

--- src/api_gorilla_mux/main.go
+++ src/api_gorilla_mux/main.go
@@ -10,12 +10,17 @@ import (
 	"github.com/gorilla/mux"
 )
 
+var usuarios = Usuarios{
+	Usuario{1, "Maria da Silva", "maria@mailtest.com", "123456"},
+	Usuario{2, "João da Silva", "joao@mailtest.com", "456789"},
+	Usuario{3, "Paulo da Silva", "paulo@mailtest.com", "789123"},
+}
+
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", index)
-	router.HandleFunc("/rota1", rota1)
-	router.HandleFunc("/usuarios", get_usuarios)
-	router.HandleFunc("/usuario/{id}", get_usuario)
+	router.HandleFunc("/", obterIndex)
+	router.HandleFunc("/usuarios", obterUsuarios)
+	router.HandleFunc("/usuario/{id}", obterUsuario)
 
 	port := "3000"
 	server := http.ListenAndServe(":"+port, router)
@@ -23,40 +28,24 @@ func main() {
 	log.Fatal(server)
 }
 
-func index(response http.ResponseWriter, request *http.Request) {
+func obterIndex(response http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(response, "API Rest Go com gorilla/mux")
 }
 
-func rota1(response http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(response, "Rota 1")
-}
-
-func get_usuarios(w http.ResponseWriter, r *http.Request) {
-	usuarios := Usuarios{
-		Usuario{1, "Maria da Silva", "maria@mailtest.com", "123456"},
-		Usuario{2, "João da Silva", "joao@mailtest.com", "456789"},
-		Usuario{3, "Paulo da Silva", "paulo@mailtest.com", "789123"},
-	}
-
+func obterUsuarios(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	enc := json.NewEncoder(w)
 	enc.Encode(usuarios)
 }
 
-func get_usuario(w http.ResponseWriter, r *http.Request) {
+func obterUsuario(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 
 	id, error := strconv.Atoi(params["id"])
 	showError(error)
 
 	id -= 1
-
-	usuarios := Usuarios{
-		Usuario{1, "João da Silva", "joao@mailtest.com", "456789"},
-		Usuario{2, "Paulo da Silva", "paulo@mailtest.com", "789123"},
-		Usuario{3, "Paulo da Silva", "paulo@mailtest.com", "789123"},
-	}
 
 	w.Header().Set("Content-Type", "application/json")
 
