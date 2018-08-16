1ab06e79d54c6d1b1937a25afba2ace2418ed5f3
Vagner Praia da Silva <vagnerpraia@gmail.com>
2018-08-16 14:36:23 -0300

    Modularizando

---
+++ src/api_gorilla_mux/resources/usuario/dao.go
@@ -0,0 +1,63 @@
+package usuario
+
+import (
+	"github.com/globalsign/mgo/bson"
+	"github.com/treinamento_go/src/api_gorilla_mux/database"
+)
+
+var session = database.GetSessionMongoDB()
+var db = session.DB("godb")
+var collection = db.C("usuarios")
+
+func getUsuariosModel() (data []Usuario, err error) {
+	var usuariosDb Usuarios
+	err = collection.Find(nil).Sort("nome").All(&usuariosDb)
+
+	if err == nil {
+		data = usuariosDb
+	}
+
+	return data, err
+}
+
+func getUsuarioModel(id string) (data Usuario, err error) {
+	if bson.IsObjectIdHex(id) {
+		idBson := bson.ObjectIdHex(id)
+
+		var usuarioDb Usuario
+		err = collection.FindId(idBson).One(&usuarioDb)
+		if err == nil {
+			data = usuarioDb
+		}
+	}
+
+	return data, err
+}
+
+func insertUsuario(usuario Usuario) (data Usuario, err error) {
+	usuarioDb := Usuario{Id: bson.NewObjectId(), Nome: usuario.Nome, Email: usuario.Email, Senha: usuario.Senha}
+	err = collection.Insert(&usuarioDb)
+	if err == nil {
+		data = usuarioDb
+	}
+
+	return data, err
+}
+
+func updateUsuario(usuario Usuario) (data Usuario, err error) {
+	if len(usuario.Id) == 12 {
+		usuarioDb := Usuario{Id: usuario.Id, Nome: usuario.Nome, Email: usuario.Email, Senha: usuario.Senha}
+		err = collection.Update(&usuarioDb.Id, &usuarioDb)
+		if err == nil {
+			data = usuarioDb
+		}
+	}
+
+	return data, err
+}
+
+var usuarios = Usuarios{
+	Usuario{bson.NewObjectId(), "Maria da Silva", "maria@mailtest.com", "123456"},
+	Usuario{bson.NewObjectId(), "João da Silva", "joao@mailtest.com", "456789"},
+	Usuario{bson.NewObjectId(), "Paulo da Silva", "paulo@mailtest.com", "789123"},
+}
--- src/api_gorilla_mux/resources/usuario/models.go
+++ src/api_gorilla_mux/resources/usuario/models.go
@@ -1,9 +1,6 @@
 package usuario
 
-import (
-	"github.com/globalsign/mgo/bson"
-	"github.com/treinamento_go/src/api_gorilla_mux/database"
-)
+import "github.com/globalsign/mgo/bson"
 
 type Usuario struct {
 	Id    bson.ObjectId `json:"_id" bson:"_id"`
@@ -13,60 +10,3 @@ type Usuario struct {
 }
 
 type Usuarios []Usuario
-
-var usuarios = Usuarios{
-	Usuario{bson.NewObjectId(), "Maria da Silva", "maria@mailtest.com", "123456"},
-	Usuario{bson.NewObjectId(), "João da Silva", "joao@mailtest.com", "456789"},
-	Usuario{bson.NewObjectId(), "Paulo da Silva", "paulo@mailtest.com", "789123"},
-}
-
-var session = database.GetSessionMongoDB()
-var db = session.DB("godb")
-var collection = db.C("usuarios")
-
-func getUsuariosModel() (data []Usuario, err error) {
-	var usuariosDb Usuarios
-	err = collection.Find(nil).Sort("nome").All(&usuariosDb)
-
-	if err == nil {
-		data = usuariosDb
-	}
-
-	return data, err
-}
-
-func getUsuarioModel(id string) (data Usuario, err error) {
-	if bson.IsObjectIdHex(id) {
-		idBson := bson.ObjectIdHex(id)
-
-		var usuarioDb Usuario
-		err = collection.FindId(idBson).One(&usuarioDb)
-		if err == nil {
-			data = usuarioDb
-		}
-	}
-
-	return data, err
-}
-
-func insertUsuario(usuario Usuario) (data Usuario, err error) {
-	usuarioDb := Usuario{Id: bson.NewObjectId(), Nome: usuario.Nome, Email: usuario.Email, Senha: usuario.Senha}
-	err = collection.Insert(&usuarioDb)
-	if err == nil {
-		data = usuarioDb
-	}
-
-	return data, err
-}
-
-func updateUsuario(usuario Usuario) (data Usuario, err error) {
-	if len(usuario.Id) == 12 {
-		usuarioDb := Usuario{Id: usuario.Id, Nome: usuario.Nome, Email: usuario.Email, Senha: usuario.Senha}
-		err = collection.Update(&usuarioDb.Id, &usuarioDb)
-		if err == nil {
-			data = usuarioDb
-		}
-	}
-
-	return data, err
-}
